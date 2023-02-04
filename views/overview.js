c392ea0f2e1edd4f71dc08b5049b8cfe757a51e2
VinzentSchmid <100967145+VinzentSchmid@users.noreply.github.com>
2023-02-04 15:20:41 +0100

    made som style changes

--- views/overview.js
+++ views/overview.js
@@ -148,7 +148,7 @@ function createWaterEntryRow(entry) {
     return `<tr class="row" id="${entry.id}">
                  <td class="linkToEntry">${entry.type}</td>
                  <td class="linkToEntry">${entry.ml}</td>
-                 ${entry.street ? `<td class="linkToEntry">${entry.street} ${entry.housenumber} ${entry.postalcode} ${entry.city} ${entry.country}</td>` : `<td class="newLocation no-hover"><select name="location" id="${entry.id}select"></select></td>`}
+                 ${entry.street ? `<td class="linkToEntry">${entry.street} ${entry.housenumber} ${entry.postalcode} ${entry.city} ${entry.country}</td>` : `<td class="newLocation"><select name="location" id="${entry.id}select"></select></td>`}
                  <td class="no-hover"><a href="/removeWaterEntry/${entry.id} "onclick="return confirm('Are you sure you want to delete this location ?')"><img class="icon" src="/public/images/delete.png" alt="delete liquid" title="delete liquid"/></a></td>
                  <td class="no-hover"><a href="/editWaterEntry/${entry.id}"><img class="icon" src="/public/images/edit.png" alt="edit liquid" title="edit liquid"/></a></td>
 
